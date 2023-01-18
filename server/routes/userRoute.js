24a5e8651b78ab36cd3e65ece93c730e4db1e574
pyropecs <praveengopu0105@gmail.com>
2023-01-18 20:45:09 +0530

    legacy

--- server/routes/userRoute.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require("express");
-const { signUpController } = require("../controllers/userControllers");
-
-const router = Router();
-router.post("/signup", signUpController);
-
-module.exports = router;
